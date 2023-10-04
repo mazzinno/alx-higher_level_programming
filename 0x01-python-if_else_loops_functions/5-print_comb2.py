54761cf3e4354d362621096d69d86aba4b44b404
mazzinno <abdennasr3@gmail.com>
2023-10-04 23:45:04 +0100

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 0:
-        print("{:02}".format(i), end="")
-    else:
-        print(", {:02}".format(i), end="")
-print()
+for i in range(99):
+    print("{:02}".format(i), end=", ")
+print(99)
