410f54fa1420b6073f4d681ea44370b48d0d0689
mazzinno <abdennasr3@gmail.com>
2023-10-05 01:59:39 +0100

    good3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(i + 1, 10):
-            if i * 10 + j != 89:
-                print("{:02}".format(i * 10 + j), end=", ")
+        if i * 10 + j != 89:
+            print("{:02}".format(i * 10 + j), end=", ")
 print(89)
