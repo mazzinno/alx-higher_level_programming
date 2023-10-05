e622ab73e9a889e6a93bca3950ea3e06aab644c1
mazzinno <abdennasr3@gmail.com>
2023-10-05 03:22:00 +0100

    task9try10

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last = (-number % 10)
+    elif number >= 0:
+        last = number % 10
+    print("{:d}".format(last), end="")
