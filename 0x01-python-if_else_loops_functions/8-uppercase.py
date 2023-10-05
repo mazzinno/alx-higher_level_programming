fccf825d7c996a6443a74c6bc933cbf6eb692ab1
mazzinno <abdennasr3@gmail.com>
2023-10-05 02:52:48 +0100

    task8try3

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,9 @@ def uppercase(str):
     i = 0
     n = 0
     while n < len(str):
-        if str[ord(i)] < 91:
+        if str[ord(str[i])] < 91:
             print("{}".format(str[i]))
             i += 1
-        elif str[(i)] > 91:
+        elif str[ord(str[i])] > 91:
             print("{}".format(str[i]))
             i += 1
