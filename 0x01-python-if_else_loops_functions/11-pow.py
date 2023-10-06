5a17f9edd0e61e2307b1b8c29551999a05108c71
mazzinno <abdennasr3@gmail.com>
2023-10-06 01:33:50 +0100

    tt

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def pow(a, b):
     return (a ** b)
+def pow(a, b):
+    return a ** b
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz ", end="")
-        elif i % 3 == 0:
-            print("Fizz ", end="")
+    for num in range(1, 101):
+        if (num % 3 == 0) and (num % 5 == 0):
+            num = 'FizzBuzz'
+        elif num % 3 == 0:
+            num = 'Fizz'
+        elif num % 5 == 0:
+            num = 'Buzz'
+        print('{}'.format(num), end=' ')
