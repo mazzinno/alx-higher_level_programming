fc8dc19c2c590b379e335e017972fb8a9b9e69b9
mazzinno <abdennasr3@gmail.com>
2023-10-08 02:19:54 +0100

    tets

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num_arg = len(sys.argv) - 1
-    print("{:d} {:s}{:s}".format(
-        num_arg,
-        "argument" if num_arg == 1 else "arguments",
-        "." if num_arg == 0 else ":"))
-    for i, arg in enumerate(sys.argv[1:]):
-        print("{:d}: {:s}".format(i + 1, arg))
+    argsnum = len(sys.argv) - 1
+    if argsnum == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    elif argsnum == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(argsnum))
+        for i in range(1, argsnum):
+            print("{}: {}".format(i, sys.argv[i]))
