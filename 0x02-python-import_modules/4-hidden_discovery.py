ff780489b2434dbcdeb729bd290e288543ffa374
mazzinno <abdennasr3@gmail.com>
2023-10-08 04:13:12 +0100

    yarbii

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    for i in dir(hidden_4):
-        if i[:2] != "__":
+import hidden_4
+
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
             print("{:s}".format(i))
+
+
+if __name__ == "__main__":
+    discovr()
