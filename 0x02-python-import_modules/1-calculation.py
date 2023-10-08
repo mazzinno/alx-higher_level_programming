acf3ef1a3c35d0b2e01420e44a25fd746e8beff2
mazzinno <abdennasr3@gmail.com>
2023-10-08 01:32:47 +0100

    te1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
 
 a = 10
 b = 5
-print("{:d} + {:d} = {:d}".format())(a, b, add(a, b))
-print("{:d} - {:d} = {:d}".format())(a, b, sub(a, b))
-print("{:d} * {:d} = {:d}".format())(a, b, mul(a, b))
-print("{:d} / {:d} = {:d}".format())(a, b, div(a, b))
+
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
