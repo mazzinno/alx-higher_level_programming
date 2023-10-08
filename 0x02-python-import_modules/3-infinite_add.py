ba05c1f40da5a24be89e60e8b512a826bbbf3326
mazzinno <abdennasr3@gmail.com>
2023-10-08 03:13:43 +0100

    test2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-    argc = len(argv) -1
-    if argc > 0:
-        sum = 0
-        for n in range(1, argc + 1):
-            sum += int(argv[n])
-        print("{:d}".format(sum))
-    else:
-        print("0")
+    from sys import argv
+argc = len(argv) -1
+if argc > 0:
+    sum = 0
+    for n in range(1, argc + 1):
+        sum += int(argv[n])
+    print("{:d}".format(sum))
+else:
+    print("0")
