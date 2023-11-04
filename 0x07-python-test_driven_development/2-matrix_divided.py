b83bad953f8bf4382f6974056b1f01067b1d8740
mazzinno <abdennasr3@gmail.com>
2023-11-04 01:34:56 +0100

    test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,23 +1,47 @@
 #!/usr/bin/python3
-"""function that divides all elements of a matrix."""
+"""
+This module divides all elements of a matrix
+"""
 
 
 def matrix_divided(matrix, div):
-    """divides two numbers"""
-    result = []
+    """
+    divides all elements of a matrix
+
+    Parameters:
+        matrix (int): the matrix
+        div (int): 2st number
+
+    Raises:
+        TypeError: if list not integers or floats
+        TypeError: each row must be the same size
+        TypeError: div must be int or float
+        ZeroDivisionError: div can’t be equal to 0
+
+    Return:
+        int: a new matrix
+    """
+    # matrix must be a list of lists of integers or floats
     for row in matrix:
         for element in row:
-            if isinstance(element, (int, float)) is False:
+            if type(element) is not int and type(element) is not float:
                 raise TypeError("matrix must be a matrix"
                                 "(list of lists) of integers/floats")
-    lenght = len(matrix[0])
-    for row in matrix:
-        if len(row) != lenght:
-            raise TypeError('Each row of the matrix must have the same size')
-    if isinstance(div, (int, float)) is False:
-        raise TypeError('div must be a number')
-    if div == 0:
-        raise ZeroDivisionError('division by zero')
 
+    # Each row of the matrix must be of the same size
+    first_row_size = len(matrix[0])
+    for row in matrix[1:]:
+        if len(row) != first_row_size:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    # div must be a number (integer or float)
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+
+    # div can’t be equal to 0
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # devide elements
     return list(map(lambda row:
                     list(map(lambda x: round(x / div, 2), row)), matrix))
