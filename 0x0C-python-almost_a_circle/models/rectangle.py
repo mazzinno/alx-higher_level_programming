f9892c9ab152d24db86d1d8c879c8ab75728635d
mazzinno <abdennasr3@gmail.com>
2023-11-17 14:58:23 +0100

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,7 +87,7 @@ class Rectangle(Base):
             print('#' * self.width)
 
     def __str__(self):
+        '''overriding the __str__ method'''
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
-dfdf
