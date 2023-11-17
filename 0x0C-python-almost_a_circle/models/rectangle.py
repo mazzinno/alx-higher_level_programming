f336e7a64531ee4dc307ae52b19fe044de293b76
mazzinno <abdennasr3@gmail.com>
2023-11-17 21:00:33 +0100

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,15 +102,16 @@ class Rectangle(Base):
                                                        self.width, self.height)
 
     def update(self, *args):
-        '''adding the public method def update(self, *args): 
+        '''adding the public method def update(self, *args):
         that assigns an argument to each attribute'''
-        argc = len(args)
-        if argc > 0:
-            try:
+        if args:
+            if len(args) >= 1:
                 self.id = args[0]
+            if len(args) >= 2:
                 self.width = args[1]
+            if len(args) >= 3:
                 self.height = args[2]
+            if len(args) >= 4:
                 self.x = args[3]
+            if len(args) >= 5:
                 self.y = args[4]
-            except:
-                pass
