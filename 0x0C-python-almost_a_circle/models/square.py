f93ee16e67925af6ac8de03568fca837d23bad33
mazzinno <abdennasr3@gmail.com>
2023-11-18 14:50:45 +0100

    test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-'''Write the class Square that inherits from Rectangle'''
+'''
+Write the class Square that inherits from Rectangle
+'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''Class Square inherits from Rectangle'''
+    '''
+    Class Square inherits from Rectangle
+    '''
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
         self.size = size
 
     def __str__(self):
-        '''printing method'''
+        '''
+        printing method
+        '''
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
