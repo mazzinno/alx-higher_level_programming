ae4b9315ac961454547edd5cfa01154f80c966c0
mazzinno <abdennasr3@gmail.com>
2023-11-18 14:49:24 +0100

    test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,4 +11,4 @@ class Square(Rectangle):
 
     def __str__(self):
         '''printing method'''
-        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size))
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
