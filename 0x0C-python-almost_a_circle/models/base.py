2fedea573ed2f9f8929e5923265c1b2a0090c479
mazzinno <abdennasr3@gmail.com>
2023-11-19 15:00:05 +0100

    tsteop

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 '''Defines a Base model class.'''
 import json
 
+
 class Base():
     '''Represent a base model.
 
@@ -23,6 +24,7 @@ class Base():
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        '''fkhater ga3 li ma3ndo khater'''
         if list_dictionaries:
             return json.dumps(list_dictionaries)
         else:
