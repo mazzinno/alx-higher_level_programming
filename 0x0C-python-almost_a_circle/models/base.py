e01113506a448fc0c4214ce2085b228f0de3666c
mazzinno <abdennasr3@gmail.com>
2023-11-19 23:14:33 +0100

    tsteop

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,3 +41,10 @@ class Base():
             else:
                 list_dictionaries = [obj.to_dictionary() for obj in list_objs]
                 json_file.write(Base.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''testest etst'''
+        if json_string is None or json_string == []:
+            return '[]'
+        return json.loads(json_string)
