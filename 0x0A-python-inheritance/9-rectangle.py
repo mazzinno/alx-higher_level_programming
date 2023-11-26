b6738cc064e085f4527c6d3717b73d3099288dcc
mazzinno <abdennasr3@gmail.com>
2023-11-26 19:00:15 +0100

    die

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-'''
-Write a class Rectangle
-that inherits from BaseGeometry (7-base_geometry.py).
-(task based on 8-rectangle.py)
-'''
-
+'''Defines a class Rectangle that inherits from BaseGeometry.'''
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    '''Represent a rectangle that inherits from BaseGeometry.'''
 
     def __init__(self, width, height):
-        super().integer_validator('width', width)
+        '''Initialize a new Rectangle.
+
+        Args:
+           width (int): The width of the new Rectangle.
+           height (int): The height of the new Rectangle.
+        '''
+        super().integer_validator("width", width)
         self.__width = width
-        super().integer_validator('height', height)
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
+        '''Return the area of the rectangle.'''
         return self.__width * self.__height
 
     def __str__(self):
-        return '[Rectangle] {}/{}'.format(self.__width, self.__height)
+        '''Return the print() and str() representation of a Rectangle.'''
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+
+        return string
