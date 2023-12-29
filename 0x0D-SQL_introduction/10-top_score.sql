2b23004b06b7e91836733c528b3ab5f4ef725709
mazzinno <abdennasr3@gmail.com>
2023-12-29 23:18:46 +0100

    done

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name FROM second_table ORDER by score DESC
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name FROM second_table WHERE score >= 10 ORDER by score DESC
