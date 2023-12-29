d84901670d345ddd98bbb26fea8fa64e7df134a2
mazzinno <abdennasr3@gmail.com>
2023-12-29 17:44:25 +0100

    done

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,2 @@
 -- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-SELECT COUNT (id)
-FROM first_table Where id = 89;
+SELECT COUNT(id) FROM first_table Where id = 89;
