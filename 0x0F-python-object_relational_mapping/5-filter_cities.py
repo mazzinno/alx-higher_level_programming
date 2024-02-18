27c1e0072e086d47af8f8034ee0b52f9318ec5a8
mazzinno <abdennasr3@gmail.com>
2024-02-18 23:47:25 +0100

    test

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"arisnkr"
-import sys
+'''
+Lists all cities 
+'''
+
 import MySQLdb
-
-
-def main():
-    user = sys.argv[1]
-    ps = sys.argv[2]
-    dbn = sys.argv[3]
-    st = sys.argv[4]
-    db = MySQLdb.connect(host="localhost", port=3306, user=user,
-                         passwd=ps, db=dbn, charset="utf8")
-    cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM cities
-                JOIN states ON cities.state_id = states.id
-                LIKE %s=states.name
-                ORDER BY cities.id ASC""", (st,))
-    mylist = cur.fetchall()
-    re = ""
-    for r in mylist:
-        re += r[0] + ", "
-    cur.close()
-    db.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    main()
+
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN\
+            states ON cities.state_id = states.id WHERE states.name=%s\
+            ORDER by cities.id ASC;",
+                (argv[4], ))
+    query_rows = cur.fetchall()
+    print(', '.join([row[1] for row in query_rows]))
+    cur.close()
+    conn.close()
