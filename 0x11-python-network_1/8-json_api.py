e2bca64ee0f2b925b8c26d7178854a8d7e3f6fcd
mazzinno <abdennasr3@gmail.com>
2024-03-04 17:23:59 +0100

    done

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 1:
         data = {'q': sys.argv[1]}
     else:
-        data = {'q':""}
+        data = {'q': ""}
     response = requests.post('http://0.0.0.0:5000/search_user', data)
     try:
         # Try to parse the response as JSON
