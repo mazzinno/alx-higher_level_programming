020dff9e4b29d6096c9ceddf21c367c026a1db71
mazzinno <abdennasr3@gmail.com>
2024-03-28 18:30:22 +0000

    done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,15 +2,15 @@
 const REQUEST = require('request');
 const URL = process.argv[2];
 REQUEST.get(URL, function (err, _, body) {
-  let counter = 0;
+  let count = 0;
   if (err) {
     console.log(err);
   }
   const data = JSON.parse(body);
-  for (let n = 0; data.results[n] !== undefined; n++) {
-    if (data.results[n].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      counter++;
+  for (let i = 0; data.results[i] !== undefined; i++) {
+    if (data.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
     }
   }
-  console.log(counter);
+  console.log(count);
 });
